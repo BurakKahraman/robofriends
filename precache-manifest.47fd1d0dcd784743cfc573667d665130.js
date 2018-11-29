ea7f5676882cbaf1a6147059a3ba1aa47ecac362
BurakKahraman <burak.kahraman02@gmail.com>
2018-11-29 17:44:53 +0100

    Updates

---
+++ precache-manifest.47fd1d0dcd784743cfc573667d665130.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1d0bd8f5b36c416e278",
+    "url": "/BurakKahraman/robofriends/static/js/runtime~main.e1d0bd8f.js"
+  },
+  {
+    "revision": "f0f77393f63890c6824a",
+    "url": "/BurakKahraman/robofriends/static/js/main.f0f77393.chunk.js"
+  },
+  {
+    "revision": "b4d7164f6e791a6f3d02",
+    "url": "/BurakKahraman/robofriends/static/js/1.b4d7164f.chunk.js"
+  },
+  {
+    "revision": "f0f77393f63890c6824a",
+    "url": "/BurakKahraman/robofriends/static/css/main.01cb558b.chunk.css"
+  },
+  {
+    "revision": "b4d7164f6e791a6f3d02",
+    "url": "/BurakKahraman/robofriends/static/css/1.6e79f4cf.chunk.css"
+  },
+  {
+    "revision": "fda6bdfef60aa67311af626e0a76dd7f",
+    "url": "/BurakKahraman/robofriends/index.html"
+  }
+];
